6fabd9832443b0e99ae3fb9429ba595855283d1c
bryanparmelee <bryan.parmelee@gmail.com>
2023-08-04 17:28:51 -0400

    Create UserContext

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,36 @@
+import { onAuthStateChangedListener } from '@/api/firebase'
+import { User } from 'firebase/auth'
+import { createContext, useState, useEffect, ReactNode, Dispatch, SetStateAction } from 'react'
+
+type UserContextType = {
+    currentUser: User | null
+    setCurrentUser: Dispatch<SetStateAction<null>>
+}
+
+type Props = {
+    children: ReactNode
+}
+
+export const UserContext = createContext<UserContextType>({
+    currentUser: null,
+    setCurrentUser: () => null
+})
+
+export const UserProvider = ({ children }: Props) => {
+    const [currentUser, setCurrentUser] = useState(null)
+    const value = {
+        currentUser,
+        setCurrentUser
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if (!user) {
+                setCurrentUser(user)
+            }
+        })
+        return unsubscribe
+    }, [])
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
