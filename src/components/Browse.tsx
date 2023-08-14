ea20e8530c5e38f2ce18486354c585a3faf097d2
Scott Gilbert <scottvtgilbert@gmail.com>
2023-08-13 22:17:13 -0400

    Adding search bar component and including in browse component

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -3,9 +3,10 @@
 import { Donation } from '../models/donation'
 //Components
 import DonationCard from './DonationCard'
+import SearchBar from './SearchBar'
 //Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faFilter} from '@fortawesome/free-solid-svg-icons'
+import { faFilter, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 //Hooks
 import React, { useEffect, useState } from 'react'
 //Libs
@@ -15,50 +16,70 @@ import styles from './Browse.module.css'
 
 const dummyDonations: Donation[] = [
     new Donation('category', 'brand', 'model', 'description', true, ['img1'], new Date(), new Date()),
-    new Donation('category', 'brand', 'model', 'description', true, ['img1'], new Date(), new Date()),
-    new Donation('category', 'brand', 'model', 'description', true, ['img1'], new Date(), new Date()),
-    new Donation('category', 'brand', 'model', 'description', true, ['img1'], new Date(), new Date()),
-    new Donation('category', 'brand', 'model', 'description', true, ['img1'], new Date(), new Date())
+    new Donation('category', 'brand', 'model', 'description', true, ['img2'], new Date(), new Date()),
+    new Donation('category', 'brand', 'model', 'description', true, ['img3'], new Date(), new Date()),
+    new Donation('category', 'brand', 'model', 'description', true, ['img4'], new Date(), new Date()),
+    new Donation('category', 'brand', 'model', 'description', true, ['img5'], new Date(), new Date())
 ]
 
 
 const Browse: React.FC = () => {
     const [donations, setDonations] = useState([] as Donation[])
+    const [isSearchVisible, setIsSearchVisible] = useState<boolean>(false)
+    const [isFilterVisible, setIsFilterVisible] = useState<boolean>(false)
+
+    function toggleSearchBar() {
+        setIsSearchVisible(prev => !prev)
+    }
+    function toggleFilters() {
+        setIsFilterVisible(prev => !prev)
+    }
 
     /**
      * On component render sets the donations state to the active donations retreived from Firebase.
      */
     useEffect(() => {
         /*Using dummy data for UI testing*/
-         setDonations(dummyDonations)
-        
-         /*
-        getActiveDonations().then((response) => {
-            setDonations(response)
-        })
-        */
+        setDonations(dummyDonations)
+
+        /*
+       getActiveDonations().then((response) => {
+           setDonations(response)
+       })
+       */
     }, [])
 
     return (
         <>
-        <div className={styles['browse__header']}>
-            <div>Tabs</div>
-            <FontAwesomeIcon icon={faFilter} />
-        </div>
-        <div className = {styles['browse__grid']}>
-            {donations.map((donation) => {
-                // An active donation must have at least one photo for display.
-                return <DonationCard 
-                category={donation.category}
-                brand={donation.brand}
-                model={donation.model}
-                description={donation.description}
-                active={donation.active}
-                images={donation.images}
-                createdAt={donation.createdAt}
-                modifiedAt={donation.modifiedAt} />
-            })}
-        </div>
+            <div className={styles['browse__header']}>
+                <div>Tabs</div>
+                <div className={styles['header__icons']}>
+                    <div onClick={toggleSearchBar}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </div>
+                    <div onClick={toggleFilters}>
+                        <FontAwesomeIcon icon={faFilter} />
+                    </div>
+                </div>
+            </div>
+            {isSearchVisible &&
+                <SearchBar />
+            }
+            <div className={styles['browse__grid']}>
+                {donations.map((donation) => {
+                    // An active donation must have at least one photo for display.
+                    return <DonationCard
+                        key={donation.images[0]}
+                        category={donation.category}
+                        brand={donation.brand}
+                        model={donation.model}
+                        description={donation.description}
+                        active={donation.active}
+                        images={donation.images}
+                        createdAt={donation.createdAt}
+                        modifiedAt={donation.modifiedAt} />
+                })}
+            </div>
         </>
     )
 }
