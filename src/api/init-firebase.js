10aa588224f7bf56d7b6d340931cded5a7e31c6a
AMViens <aviens66@gmail.com>
2023-08-24 23:46:43 -0400

    Create init-firebase.js

---
+++ src/api/init-firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {getFirestore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfv1GI3fwfAin17N_Pf2G9fe8PPBa1UPs",
+  authDomain: "baby-equipment-exchange.firebaseapp.com",
+  projectId: "baby-equipment-exchange",
+  storageBucket: "baby-equipment-exchange.appspot.com",
+  messagingSenderId: "375825360663",
+  appId: "1:375825360663:web:2307ccd57b31bf1d74b952"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+export const db = getFirestore(app);
