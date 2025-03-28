608edb72da8bfe34d6f41a24e77b1731b2538274
Bryan Parmelee <90424427+bryanparmelee@users.noreply.github.com>
2025-03-27 20:32:02 -0400

    Update component.ts

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,11 +1,11 @@
-import { mount } from 'cypress/react18';
+//import { mount } from 'cypress/react18';
 
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            mount: typeof mount;
-        }
-    }
-}
+//declare global {
+//    namespace Cypress {
+//        interface Chainable {
+//            mount: typeof mount;
+//        }
+//    }
+//}
 
-Cypress.Commands.add('mount', mount);
+//Cypress.Commands.add('mount', mount);
