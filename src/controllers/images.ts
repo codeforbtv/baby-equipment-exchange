3d2efd55e48f12172249d022855b421e062c056f
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-05-14 16:41:46 -0400

    Refactor append/delete image thumbnails

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -2,7 +2,11 @@ import { SetStateAction, Dispatch } from 'react';
 
 type Images = File[] | null | undefined;
 
-export function appendImagesToState(images: Images, setImages: (images: File[]) => void, event: React.ChangeEvent<HTMLInputElement>) {
+export function appendImagesToState(
+    images: Images,
+    setImages: Dispatch<SetStateAction<File[] | null | undefined>>,
+    event: React.ChangeEvent<HTMLInputElement>
+) {
     if (event.target.files == null || event.target.files.length == 0) {
         return;
     }
@@ -27,15 +31,6 @@ export function appendImagesToState(images: Images, setImages: (images: File[]) 
 
 export function removeImageFromState(images: Images, setImages: Dispatch<SetStateAction<File[] | null | undefined>>, fileToRemove: File) {
     if (images) {
-        let updatedImages;
-        for (let index = 0; index < images.length; index++) {
-            if (images[index].name === fileToRemove.name) {
-                //if the relative path of the file at index `index`
-                //matches the relative path of `fileToRemove`
-                //remove one element at `index` from images
-                updatedImages = images.toSpliced(index, 1);
-            }
-        }
-        setImages(updatedImages);
+        setImages(images.filter((image) => image.name !== fileToRemove.name));
     }
 }
