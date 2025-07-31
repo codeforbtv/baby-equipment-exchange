3117a94e92abf09d5101edef32cf5ca1b2b01242
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-07-31 16:21:03 -0400

    Create inventory item component

--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -1,14 +1,42 @@
+'use client';
+
+//Components
 import { ImageListItem, ImageListItemBar, IconButton } from '@mui/material';
 
+// Icons
+import InfoIcon from '@mui/icons-material/Info';
+
+//Hooks
+import { useRequestedInventoryContext } from '@/contexts/RequestedInventoryContext';
+
+//Types
 import { InventoryItemCardProps } from '@/types/DonationTypes';
+
+//Style
+import styles from './Card.module.css';
 
 const InventoryItemCard = (inventoryItem: InventoryItemCardProps) => {
     const image = inventoryItem.images ? inventoryItem.images[0] : '';
 
+    const { addRequestedInventoryItem } = useRequestedInventoryContext();
+
     return (
-        <ImageListItem key={inventoryItem.id}>
-            <img src={image} style={{ width: '100%', height: '100%', objectFit: 'fill' }} alt={`${inventoryItem.brand} ${inventoryItem.model}`} />
-            <ImageListItemBar title={inventoryItem.brand} subtitle={inventoryItem.category} actionIcon={<IconButton></IconButton>}></ImageListItemBar>
+        <ImageListItem key={inventoryItem.id} className={styles['grid__item']}>
+            <img
+                src={image}
+                style={{ width: '100%', height: '100%', objectFit: 'fill' }}
+                alt={`${inventoryItem.brand} ${inventoryItem.model}`}
+                onClick={() => console.log(`You clicked ${inventoryItem.id}`)}
+            />
+            <ImageListItemBar
+                title={inventoryItem.brand}
+                subtitle={inventoryItem.category}
+                actionIcon={
+                    <IconButton sx={{ color: 'rgb(255, 255, 255)' }} aria-label={`details about ${inventoryItem.brand} ${inventoryItem.model}`} size="large">
+                        <InfoIcon />
+                    </IconButton>
+                }
+            ></ImageListItemBar>
         </ImageListItem>
     );
 };
