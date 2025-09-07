5748af294c309f0bc7afdc430e8b37870790275c
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-09-07 12:02:17 -0400

    Update donation details page

--- src/app/donations/[id]/page.tsx
+++ src/app/donations/[id]/page.tsx
@@ -1,19 +1,27 @@
 'use client';
-import { getDonationById } from '@/api/firebase-donations';
-import Loader from '@/components/Loader';
-import { useEffect, useState } from 'react';
 
+//Hooks
+import { useEffect, useState } from 'react';
+//APi
 import { addErrorEvent } from '@/api/firebase';
+import { getDonationById } from '@/api/firebase-donations';
+//Components
+import Loader from '@/components/Loader';
+//Styles
+
+//Types
 import { Donation } from '@/models/donation';
+import ProtectedAdminRoute from '@/components/ProtectedAdminRoute';
 
 export default function DonationDetails({ params }: { params: { id: string } }) {
-    const [donation, setDonation] = useState<Donation | null>(null);
+    const [donationDetails, setDonationDetails] = useState<Donation | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
 
     async function fetchDonation(id: string) {
         try {
             const donationToView = await getDonationById(id);
-            setDonation(donationToView);
+            setDonationDetails(donationToView);
             setIsLoading(false);
         } catch (error: any) {
             addErrorEvent(`Fetch donation by ID`, error);
@@ -24,22 +32,20 @@ export default function DonationDetails({ params }: { params: { id: string } }) 
         fetchDonation(params.id);
     }, []);
 
-    if (isLoading) {
-        return <Loader />;
-    }
-    if (!isLoading && donation === null) {
-        return <p>You are not authorized to view this donation.</p>;
-    }
-    if (!isLoading && donation !== null) {
-        return (
+    return (
+        <ProtectedAdminRoute>
             <div className="page--header">
-                <h1>Donation Details</h1>
-                <div className="content--container">
-                    <h2>{donation.brand}</h2>
-                    <h3>{donation.model}</h3>
-                    <p>{donation.description}</p>
-                </div>
+                {isEditMode ? <h1>Donation Details</h1> : <h1>Edit Donation</h1>}
+                {isLoading && <Loader />}
+                {!isLoading && donationDetails === null && <p>Donation not found</p>}
+                {!isLoading && donationDetails !== null && !isEditMode && (
+                    <div className="content--container">
+                        <h2>{donationDetails.brand}</h2>
+                        <h3>{donationDetails.model}</h3>
+                        <p>{donationDetails.description}</p>
+                    </div>
+                )}
             </div>
-        );
-    }
+        </ProtectedAdminRoute>
+    );
 }
