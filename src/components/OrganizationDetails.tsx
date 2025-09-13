63b3c055628ccff3766d111f9326ebc8ffbfa765
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-09-13 19:12:44 -0400

    Re-fetch orgs when one is updated

--- src/components/OrganizationDetails.tsx
+++ src/components/OrganizationDetails.tsx
@@ -20,13 +20,13 @@ import { orgTags, OrganizationTagKeys, IOrganization } from '@/models/organizati
 type OrganizationDetailsProps = {
     id: string;
     setIdToDisplay?: Dispatch<SetStateAction<string | null>>;
-    setOrgsUpdated: Dispatch<SetStateAction<boolean>>;
+    setOrgsUpdated?: Dispatch<SetStateAction<boolean>>;
 };
 
 const tagNames: OrganizationTagKeys[] = Object.keys(orgTags) as OrganizationTagKeys[];
 
 const OrganizationDetails = (props: OrganizationDetailsProps) => {
-    const { id, setIdToDisplay } = props;
+    const { id, setIdToDisplay, setOrgsUpdated } = props;
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isEditMode, setIsEditMode] = useState<boolean>(false);
@@ -48,14 +48,10 @@ const OrganizationDetails = (props: OrganizationDetailsProps) => {
         fetchOrganizationById(id);
     }, []);
 
-    useEffect(() => {
-        console.log(organizationDetails);
-    }, [organizationDetails]);
-
     return (
         <ProtectedAdminRoute>
             <div className="page--header">
-                {!isEditMode ? <h1>Organization Details</h1> : <h1>Edit Organization</h1>}
+                {!isEditMode ? <h3>Organization Details</h3> : <h3>Edit Organization</h3>}
                 {setIdToDisplay && (
                     <IconButton onClick={() => setIdToDisplay(null)}>
                         <ArrowBackIcon />
@@ -115,7 +111,12 @@ const OrganizationDetails = (props: OrganizationDetailsProps) => {
                     </div>
                 )}
                 {!isLoading && organizationDetails && isEditMode && (
-                    <EditOrganization organizationDetails={organizationDetails} setIsEditMode={setIsEditMode} fetchDonationDetails={fetchOrganizationById} />
+                    <EditOrganization
+                        organizationDetails={organizationDetails}
+                        setIsEditMode={setIsEditMode}
+                        fetchDonationDetails={fetchOrganizationById}
+                        setOrgsUpdated={setOrgsUpdated}
+                    />
                 )}
             </div>
         </ProtectedAdminRoute>
