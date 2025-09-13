f42b76c7545caccb12ba95b6fdb2fdae175ade6d
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-09-13 13:40:33 -0400

    Add create donation to dahboard

--- src/app/donations/page.tsx
+++ src/app/donations/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 //Components
-import { ImageList } from '@mui/material';
+import { Button, ImageList } from '@mui/material';
 import DonationCard from '@/components/DonationCard';
 //Styles
 import '@/styles/globalStyles.css';
 import styles from '@/components/Browse.module.css';
 //Types
 import { Donation } from '@/models/donation';
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import DonationDetails from '@/components/DonationDetails';
 import ProtectedAdminRoute from '@/components/ProtectedAdminRoute';
+import DonationForm from '@/components/DonationForm';
 
 type DonationsProps = {
     donations: Donation[];
@@ -19,16 +20,26 @@ type DonationsProps = {
 const Donations = (props: DonationsProps) => {
     const { donations } = props;
     const [idToDisplay, setIdToDisplay] = useState<string | null>(null);
+    const [showForm, setShowForm] = useState<boolean>(false);
+
+    const handleShowForm = () => {
+        //Close details if open
+        setIdToDisplay(null);
+        setShowForm(true);
+    };
 
     return (
         <ProtectedAdminRoute>
-            {idToDisplay ? (
-                <DonationDetails id={idToDisplay} setIdToDisplay={setIdToDisplay} />
-            ) : (
+            {idToDisplay && <DonationDetails id={idToDisplay} setIdToDisplay={setIdToDisplay} />}
+            {showForm && <DonationForm setShowForm={setShowForm} />}
+            {!idToDisplay && !showForm && (
                 <>
                     <div className="page--header">
                         <h1>Donations</h1>
                     </div>
+                    <Button variant="contained" type="button" onClick={handleShowForm}>
+                        Add donation
+                    </Button>
                     <ImageList className={styles['browse__grid']}>
                         {donations.map((donation) => (
                             <DonationCard key={donation.id} donation={donation} setIdToDisplay={setIdToDisplay} />
