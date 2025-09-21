84d6da1dc0f8aa7cda74f3584141cd8333a70f94
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-09-21 15:58:37 -0400

    Uupdate styles

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,20 +3,32 @@
 //Hooks
 import { useRouter } from 'next/navigation';
 //Components
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 //Styles
 import homeStyles from '@/app/HomeStyles.module.css';
+import '@/styles/globalStyles.css';
 
 const HomePage = () => {
     const router = useRouter();
     return (
-        <div className={homeStyles['home--header']}>
-            <h2>Welcome to the Baby Equipment Exchange!</h2>
-            <p>A 100% volunteer led initiative to provide durable equipment to families in need through partner referrals and community donations.</p>
-            <Button variant="contained" type="button" aria-label="Make a Donation" onClick={() => router.push('/donate')}>
+        <>
+            <div className={homeStyles['home--header']}>
+                <h2>Welcome to the Baby Equipment Exchange!</h2>
+                <p>A 100% volunteer led initiative to provide durable equipment to families in need through partner referrals and community donations.</p>
+            </div>
+            <Button variant="contained" size="large" type="button" aria-label="Make a Donation" onClick={() => router.push('/donate')}>
                 Make a Donation
             </Button>
-        </div>
+            <Typography variant="body2">No account required!</Typography>
+            <div className={homeStyles['info--box']}>
+                <p>Are you an existing partner? </p>
+                <div className={homeStyles['btn--group']}>
+                    <Button variant="text">Login</Button>
+                    <Typography variant="body2">or</Typography>
+                    <Button variant="text">Create an account</Button>
+                </div>
+            </div>
+        </>
     );
 };
 
