cfa62617a4204f1344af0dde4aa69bbdad3bfb9e
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-12-01 18:16:38 -0500

    Fetch categories from server

--- src/components/DonationForm.tsx
+++ src/components/DonationForm.tsx
@@ -6,18 +6,21 @@ import { usePendingDonationsContext } from '@/contexts/PendingDonationsContext';
 //Components
 import ImageThumbnail from './ImageThumbnail';
 import InputContainer from '@/components/InputContainer';
-import { Autocomplete, Box, Button, NativeSelect, TextField } from '@mui/material';
+import { Autocomplete, Box, Button, TextField, Typography } from '@mui/material';
 //Icons
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 //Api
 import { appendImagesToState, removeImageFromState } from '@/controllers/images';
-import { categories } from '@/data/html';
 //styles
 import styles from './DonationForm.module.css';
 import '../styles/globalStyles.css';
 //Types
 import { DonationFormData } from '@/types/DonationTypes';
 import CustomDialog from './CustomDialog';
+import { Category } from '@/models/category';
+import { addErrorEvent } from '@/api/firebase';
+import Loader from './Loader';
+import { getAllCategories } from '@/api/firebase-categories';
 
 export type DonationFormProps = {
     setShowForm: Dispatch<SetStateAction<boolean>>;
@@ -34,15 +37,33 @@ export default function DonationForm(props: DonationFormProps) {
     const [images, setImages] = useState<File[] | null>();
     const [imageElements, setImageElements] = useState<ReactElement[]>([]);
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [categories, setCategories] = useState<Category[] | null>(null);
 
     const { addPendingDonation, pendingDonations } = usePendingDonationsContext();
+
+    const fetchCategories = async (): Promise<void> => {
+        try {
+            setIsLoading(true);
+            const categoriesResult = await getAllCategories();
+            setCategories(categoriesResult);
+        } catch (error) {
+            addErrorEvent('Error fetching all categories: ', error);
+            throw error;
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     const isDisabled =
         !images || formData.category?.length === 0 || formData.brand?.length === 0 || formData.model?.length === 0 || formData.description?.length === 0;
 
     const isCategoryActive = (category: string) => {
-        const currentCategory = categories.find((cat) => cat.name === category);
-        return !currentCategory?.active;
+        if (categories) {
+            const currentCategory = categories.find((cat) => cat.name === category);
+            return !currentCategory?.active;
+        }
+        return false;
     };
 
     useEffect(() => {
@@ -122,86 +143,101 @@ export default function DonationForm(props: DonationFormProps) {
         props.setShowForm(false);
     }
 
+    useEffect(() => {
+        if (!categories) fetchCategories();
+    }, []);
+
     return (
-        <div className="content--container">
-            <p>Enter item details:</p>
-            <Box component="form" className={styles['form']}>
-                <Box className={styles['form__section--left']}>
-                    <Box display={'flex'} flexDirection={'column'} gap={1}>
-                        <Autocomplete
-                            sx={{ maxWidth: '88%' }}
-                            disablePortal
-                            options={categories.map((option) => option.name)}
-                            getOptionDisabled={isCategoryActive}
-                            renderInput={(params) => <TextField {...params} label="Category" />}
-                            value={formData.category}
-                            onChange={handleCategoryChange}
-                            aria-label="Category"
-                        />
-                        <TextField
-                            type="text"
-                            label="Brand"
-                            name="brand"
-                            id="brand"
-                            placeholder=" Brand"
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
-                            value={formData.brand ? formData.brand : ''}
-                        ></TextField>
-                        <TextField
-                            type="text"
-                            label="Model"
-                            name="model"
-                            id="model"
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
-                            value={formData.model ? formData.model : ''}
-                        ></TextField>
-                        <TextField
-                            multiline={true}
-                            name="description"
-                            label="Description"
-                            rows={12}
-                            placeholder="Key details might include: special features, accessories, how the item works, ease of cleaning, size, and/or information about missing or damaged parts"
-                            id="description"
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
-                            value={formData.description ? formData.description : ''}
-                        />
+        <>
+            {isLoading && <Loader />}
+            {!isLoading && !categories && <Typography variant="body1">Could not load donation form. Please try again later.</Typography>}
+            {!isLoading && categories && (
+                <div className="content--container">
+                    <p>Enter item details:</p>
+                    <Box component="form" className={styles['form']}>
+                        <Box className={styles['form__section--left']}>
+                            <Box display={'flex'} flexDirection={'column'} gap={1}>
+                                <Autocomplete
+                                    sx={{ maxWidth: '88%' }}
+                                    disablePortal
+                                    options={categories.map((option) => option.name)}
+                                    getOptionDisabled={isCategoryActive}
+                                    renderInput={(params) => <TextField {...params} label="Category" />}
+                                    value={formData.category}
+                                    onChange={handleCategoryChange}
+                                    aria-label="Category"
+                                />
+                                <TextField
+                                    type="text"
+                                    label="Brand"
+                                    name="brand"
+                                    id="brand"
+                                    placeholder=" Brand"
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                                    value={formData.brand ? formData.brand : ''}
+                                ></TextField>
+                                <TextField
+                                    type="text"
+                                    label="Model"
+                                    name="model"
+                                    id="model"
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                                    value={formData.model ? formData.model : ''}
+                                ></TextField>
+                                <TextField
+                                    multiline={true}
+                                    name="description"
+                                    label="Description"
+                                    rows={12}
+                                    placeholder="Key details might include: special features, accessories, how the item works, ease of cleaning, size, and/or information about missing or damaged parts"
+                                    id="description"
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                                    value={formData.description ? formData.description : ''}
+                                />
+                            </Box>
+                        </Box>
+                        <Box className={styles['form__section--right']}>
+                            <InputContainer for="images" label="Upload images (required)">
+                                <div className={styles['image-uploader__container']}>
+                                    <div className={styles['image-uploader__display']}>{imageElements && imageElements}</div>
+                                    <div className={styles['image-uploader__input']}>
+                                        <label id="labelForImages" htmlFor="images">
+                                            <input
+                                                required
+                                                type="file"
+                                                id="images"
+                                                name="images"
+                                                accept="image/*"
+                                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => appendImagesToState(images, setImages, event)}
+                                                multiple
+                                            />
+                                            <Button variant="contained" component="span" className={styles['form-btn']} endIcon={<AddPhotoAlternateIcon />}>
+                                                Add Image
+                                            </Button>
+                                        </label>
+                                    </div>
+                                </div>
+                            </InputContainer>
+                        </Box>
+                        <Box className={styles['form__section--bottom']}>
+                            <Button variant="contained" fullWidth={false} size="medium" type="button" onClick={handleAddPendingDonation} disabled={isDisabled}>
+                                Add item
+                            </Button>
+                            {pendingDonations.length > 0 && (
+                                <Button variant="outlined" fullWidth={false} size="medium" type="button" onClick={handleCancel}>
+                                    Cancel
+                                </Button>
+                            )}
+                        </Box>
                     </Box>
-                </Box>
-                <Box className={styles['form__section--right']}>
-                    <InputContainer for="images" label="Upload images (required)">
-                        <div className={styles['image-uploader__container']}>
-                            <div className={styles['image-uploader__display']}>{imageElements && imageElements}</div>
-                            <div className={styles['image-uploader__input']}>
-                                <label id="labelForImages" htmlFor="images">
-                                    <input
-                                        required
-                                        type="file"
-                                        id="images"
-                                        name="images"
-                                        accept="image/*"
-                                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => appendImagesToState(images, setImages, event)}
-                                        multiple
-                                    />
-                                    <Button variant="contained" component="span" className={styles['form-btn']} endIcon={<AddPhotoAlternateIcon />}>
-                                        Add Image
-                                    </Button>
-                                </label>
-                            </div>
-                        </div>
-                    </InputContainer>
-                </Box>
-                <Box className={styles['form__section--bottom']}>
-                    <Button variant="contained" fullWidth={false} size="medium" type="button" onClick={handleAddPendingDonation} disabled={isDisabled}>
-                        Add item
-                    </Button>
-                    {pendingDonations.length > 0 && (
-                        <Button variant="outlined" fullWidth={false} size="medium" type="button" onClick={handleCancel}>
-                            Cancel
-                        </Button>
-                    )}
-                </Box>
-            </Box>
-            <CustomDialog isOpen={isOpen} onClose={handleClose} title="Image required" content="You must provide at least one image for your donation." />
-        </div>
+                    <CustomDialog
+                        isOpen={isOpen}
+                        onClose={handleClose}
+                        title="Image required"
+                        content="You must provide at least one image for your donation."
+                    />
+                </div>
+            )}
+        </>
     );
 }
