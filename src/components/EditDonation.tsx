a9a0a4a0c33ddcb2fdae731c2c5d57f4fe330acd
Bryan Parmelee <bryan.parmelee@gmail.com>
2025-12-14 16:53:57 -0500

    Add input container to form

--- src/components/EditDonation.tsx
+++ src/components/EditDonation.tsx
@@ -1,21 +1,26 @@
 'use client';
 
 //Hooks
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, ReactElement, SetStateAction, useEffect, useState } from 'react';
 //API
 import { updateDonation } from '@/api/firebase-donations';
 import { addErrorEvent } from '@/api/firebase';
 import { getAllCategories, getTagNumber } from '@/api/firebase-categories';
+import { appendImagesToState, removeImageFromState } from '@/controllers/images';
 //Components
 import ProtectedAdminRoute from './ProtectedAdminRoute';
 import Loader from './Loader';
 import { Paper, Box, TextField, Button, Stack, Typography, Autocomplete } from '@mui/material';
 import CustomDialog from './CustomDialog';
+//Icons
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 //Styles
 import '@/styles/globalStyles.css';
+import styles from '@/components/DonationForm.module.css';
 //Types
 import { IDonation } from '@/models/donation';
 import { Category } from '@/models/category';
+import InputContainer from './InputContainer';
 
 type EditDonationProps = {
     donationDetails: IDonation;
@@ -36,6 +41,9 @@ const EditDonation = (props: EditDonationProps) => {
     const [newModel, setNewModel] = useState<string>(model);
     const [newDescription, setNewDescription] = useState<string>(description ?? '');
     const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+    const [images, setImages] = useState<File[] | null>();
+    const [imageElements, setImageElements] = useState<ReactElement[]>([]);
 
     const fetchCategories = async (): Promise<void> => {
         try {
@@ -101,66 +109,94 @@ const EditDonation = (props: EditDonationProps) => {
                 {isLoading && <Loader />}
                 {!isLoading && !categories && <Typography variant="body1">Could not load edit donation form. Please try again later. </Typography>}
                 {!isLoading && categories && (
-                    <Box component="form" gap={3} display={'flex'} flexDirection={'column'} className="form--container" onSubmit={handleSubmitUpdatedDonation}>
-                        <Autocomplete
-                            sx={{ maxWidth: { sm: '88%', xs: '80%' } }}
-                            disablePortal
-                            options={categories.map((option) => option.name)}
-                            renderInput={(params) => <TextField {...params} label="Category" />}
-                            value={newCategory}
-                            onChange={handleCategoryChange}
-                            aria-label="Category"
-                        />
-                        {status !== 'rejected' && (
-                            <Stack direction="row" spacing={2}>
-                                <TextField
-                                    type="text"
-                                    label="Tag Number"
-                                    name="tagNumber"
-                                    id="tagNumber"
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewTagnumber(event.target.value)}
-                                    value={newTagNumber}
-                                />
-                                <Button variant="text" type="button" onClick={assignTagNumber}>
-                                    Generate New Tag Number
-                                </Button>
-                            </Stack>
-                        )}
+                    <Box component="form" className={styles['form']} onSubmit={handleSubmitUpdatedDonation}>
+                        <Box className={styles['form__section--left']} gap={3} display={'flex'} flexDirection={'column'}>
+                            <Autocomplete
+                                sx={{ maxWidth: { sm: '88%', xs: '80%' } }}
+                                disablePortal
+                                options={categories.map((option) => option.name)}
+                                renderInput={(params) => <TextField {...params} label="Category" />}
+                                value={newCategory}
+                                onChange={handleCategoryChange}
+                                aria-label="Category"
+                            />
+                            {status !== 'rejected' && (
+                                <Stack direction="row" spacing={2}>
+                                    <TextField
+                                        type="text"
+                                        label="Tag Number"
+                                        name="tagNumber"
+                                        id="tagNumber"
+                                        onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewTagnumber(event.target.value)}
+                                        value={newTagNumber}
+                                    />
+                                    <Button variant="text" type="button" onClick={assignTagNumber}>
+                                        Generate New Tag Number
+                                    </Button>
+                                </Stack>
+                            )}
 
-                        <TextField
-                            type="text"
-                            label="Brand"
-                            name="brand"
-                            id="brand"
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewBrand(event.target.value)}
-                            value={newBrand}
-                            required
-                        />
-                        <TextField
-                            type="text"
-                            label="Model"
-                            name="model"
-                            id="model"
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewModel(event.target.value)}
-                            value={newModel}
-                            required
-                        />
-                        <TextField
-                            multiline={true}
-                            type="text"
-                            label="Description"
-                            name="description"
-                            id="description"
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewDescription(event.target.value)}
-                            value={newDescription}
-                            required
-                        />
-                        <Button variant="contained" type="submit">
-                            Save changes
-                        </Button>
-                        <Button variant="outlined" type="button" onClick={() => setIsEditMode(false)}>
-                            Cancel
-                        </Button>
+                            <TextField
+                                type="text"
+                                label="Brand"
+                                name="brand"
+                                id="brand"
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewBrand(event.target.value)}
+                                value={newBrand}
+                                required
+                            />
+                            <TextField
+                                type="text"
+                                label="Model"
+                                name="model"
+                                id="model"
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewModel(event.target.value)}
+                                value={newModel}
+                                required
+                            />
+                            <TextField
+                                multiline={true}
+                                type="text"
+                                label="Description"
+                                name="description"
+                                id="description"
+                                rows={4}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>): void => setNewDescription(event.target.value)}
+                                value={newDescription}
+                                required
+                            />
+                        </Box>
+                        <Box className={styles['form__section--right']}>
+                            <InputContainer for="images" label="Images">
+                                <div className={styles['image-uploader__container']}>
+                                    <div className={styles['image-uploader__display']}>{imageElements && imageElements}</div>
+                                    <div className={styles['image-uploader__input']}>
+                                        <label id="labelForImages" htmlFor="images">
+                                            <input
+                                                required
+                                                type="file"
+                                                id="images"
+                                                name="images"
+                                                accept="image/*"
+                                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => appendImagesToState(images, setImages, event)}
+                                                multiple
+                                            />
+                                            <Button variant="contained" component="span" className={styles['form-btn']} endIcon={<AddPhotoAlternateIcon />}>
+                                                Add Image
+                                            </Button>
+                                        </label>
+                                    </div>
+                                </div>
+                            </InputContainer>
+                        </Box>
+                        <Box className={styles['form__section--bottom']}>
+                            <Button variant="contained" type="submit">
+                                Save changes
+                            </Button>
+                            <Button variant="outlined" type="button" onClick={() => setIsEditMode(false)}>
+                                Cancel
+                            </Button>
+                        </Box>
                     </Box>
                 )}
             </Paper>
