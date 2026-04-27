bcc3333c093fb1ac935fa7106aa0618c878b8d2a
ShariqAli-Dev <shariqali.dev@gmail.com>
2026-04-26 22:49:43 -0400

    updated next firebase env -> server side env

--- src/api/firebaseAdmin.ts
+++ src/api/firebaseAdmin.ts
@@ -45,7 +45,7 @@ export async function initAdmin() {
         return initializeApp();
     } else {
         const credentials: ServiceAccount = {
-            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY
         };
