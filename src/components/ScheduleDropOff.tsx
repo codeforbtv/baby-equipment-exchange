d4ed19092c013ad2c273da0355aa625e9c87ca89
NathanWEdwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-05-09 20:24:23 -0400

    Staging: order flow changes - Make scheduling links optional, Sort all notifications by Bulk ID (#326)
    * feature(order flow changes): scheduling pickup/dropoff links should be optional.
    
    * feature(order flow changes): sort donations awaiting pickup by bulk ID.
    
    * feature(order flow changes): remove ScheduleDropOff.isDisabled expression.

--- src/components/ScheduleDropOff.tsx
+++ src/components/ScheduleDropOff.tsx
@@ -41,8 +41,6 @@ const ScheduleDropOff = (props: ScheduleDropOffProps) => {
 
     const router = useRouter();
 
-    const isDisabled = acceptedDonations && acceptedDonations.length > 0 ? !inviteUrl : false;
-
     let donorEmail = '';
     let donorName = '';
     if (acceptedDonations && acceptedDonations.length > 0) {
@@ -185,7 +183,7 @@ const ScheduleDropOff = (props: ScheduleDropOffProps) => {
                                     </InputLabel>
                                     <NativeSelect variant="outlined" name="location" id="location" onChange={handleSelect} value={inviteUrl}>
                                         <option value="" disabled>
-                                            Select Calendar
+                                            Select Calendar (Optional)
                                         </option>
                                         {events &&
                                             events.map((event, index) => {
@@ -201,7 +199,7 @@ const ScheduleDropOff = (props: ScheduleDropOffProps) => {
                                 </FormControl>
                             )}
                             <Box sx={{ marginTop: '2em' }} display={'flex'} gap={2}>
-                                <Button onClick={handleSubmit} disabled={isDisabled} variant="contained">
+                                <Button onClick={handleSubmit} variant="contained">
                                     Send Email
                                 </Button>
                                 <Button variant="outlined" type="button" onClick={() => setOpenScheduler(false)}>
--- src/components/SchedulePickup.tsx
+++ src/components/SchedulePickup.tsx
@@ -136,7 +136,7 @@ const SchedulePickup = (props: SchedulePickupProps) => {
                                 </InputLabel>
                                 <NativeSelect variant="outlined" name="location" id="location" onChange={handleSelect} value={inviteUrl}>
                                     <option value="" disabled>
-                                        Select Calendar
+                                        Select Calendar (Optional)
                                     </option>
                                     {events &&
                                         events.map((event, index) => {
@@ -151,7 +151,7 @@ const SchedulePickup = (props: SchedulePickupProps) => {
                                 </NativeSelect>
                             </FormControl>
                             <Box sx={{ marginTop: '2em' }} display={'flex'} gap={2}>
-                                <Button variant="contained" disabled={!inviteUrl} onClick={handleSubmit}>
+                                <Button variant="contained" onClick={handleSubmit}>
                                     Send Email
                                 </Button>
                                 <Button variant="outlined" onClick={() => setShowScheduler(false)}>
