7526abaccf8b70d075251de1fb25bc4b1c037aa3
shariqali-dev <shariqali.dev@gmail.com>
2026-05-13 20:44:47 -0400

    unused var

--- src/api/firebase-users.ts
+++ src/api/firebase-users.ts
@@ -21,8 +21,6 @@ import {
     User,
     UserCredential
 } from 'firebase/auth';
-//API
-import { getAuthUserById } from './firebaseAdmin';
 // Models
 import { IUser, UserCollection } from '@/models/user';
 import { Event, IEvent } from '@/models/event';
@@ -119,33 +117,6 @@ export async function getDbUser(uid: string): Promise<UserCollection> {
     return Promise.reject();
 }
 
-//returns Auth User and db User details combined
-export async function getUserDetails(uid: string): Promise<IUser> {
-    try {
-        const [authUser, dbUser] = await Promise.all([getAuthUserById(uid), getDbUser(uid)]);
-        const userDetails: IUser = {
-            uid: authUser.uid,
-            email: authUser.email,
-            displayName: authUser.displayName,
-            disabled: authUser.disabled,
-            metadata: authUser.metadata,
-            customClaims: authUser.customClaims,
-            phoneNumber: dbUser.phoneNumber,
-            requestedItems: dbUser.requestedItems,
-            distributedItems: dbUser.distributedItems,
-            notes: dbUser.notes,
-            organization: dbUser.organization,
-            title: dbUser.title,
-            termsAccepted: dbUser.termsAccepted,
-            createdAt: dbUser.createdAt,
-            modifiedAt: dbUser.modifiedAt
-        };
-        return userDetails;
-    } catch (error) {
-        addErrorEvent('Get User Details', error);
-    }
-    return Promise.reject();
-}
 
 export async function getUsersNotifications(): Promise<IUser[]> {
     const users: IUser[] = [];
@@ -173,15 +144,6 @@ export async function getUserId(): Promise<string> {
     return currentUser ?? Promise.reject();
 }
 
-export async function getUserEmailById(id: string): Promise<string> {
-    try {
-        const user = await getAuthUserById(id);
-        if (user.email) return user.email;
-    } catch (error) {
-        addErrorEvent('Get user email by ID', error);
-    }
-    return Promise.reject();
-}
 
 export async function signInAuthUserWithEmailAndPassword(email: string, password: string): Promise<null | User> {
     if (!email || !password) {
