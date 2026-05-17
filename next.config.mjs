9fe8556ddcc33684c46c5d80bcfa5a3bb8874a16
shariqali-dev <shariqali.dev@gmail.com>
2026-05-16 22:19:52 -0400

    svg warning

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,9 @@ const nextConfig = {
         ...emulatorEnv
     },
     images: {
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
         remotePatterns: [
             {
                 protocol: 'https',
