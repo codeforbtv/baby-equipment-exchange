292024535bc8f87200e76b147b31dcc781858707
Nathan Edwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-05-16 21:24:20 -0400

    fix(donation-flow): donate should always appear in the nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@
 import Link from 'next/link';
 import NavMenu from './NavMenu';
 //Hooks
-import { useEffect, useState } from 'react';
-import { useUserContext } from '@/contexts/UserContext';
+import { useState } from 'react';
 //Icons
 import StrollerOutlinedIcon from '@mui/icons-material/StrollerOutlined';
 
