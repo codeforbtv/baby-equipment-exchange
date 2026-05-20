e66741fd6dfdcde695e9261c63a4030df67868eb
Nathan Edwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-05-20 04:48:09 -0400

    fix(email): update calendly.ts

--- src/api/calendly.ts
+++ src/api/calendly.ts
@@ -2,9 +2,11 @@
 
 import 'server-only';
 
-const API_KEY = process.env.CALENDLY_API_KEY;
+const API_KEY = process.env.CALENDLY_API_KEY ?? '';
+const CALENDLY_ORG_ID = process.env.CALENDLY_ORGANIZATION ?? '';
+const organization = encodeURIComponent(`https://api.calendly.com/organizations/${CALENDLY_ORG_ID}`);
 const url =
-    'https://api.calendly.com/event_types?active=true&organization=https%3A%2F%2Fapi.calendly.com%2Forganizations%2F48b74e58-cecf-4fd6-9594-63401556c5c9';
+    `https://api.calendly.com/event_types?active=true&organization=${organization}`;
 import { EventType } from '@/types/CalendlyTypes';
 import { addErrorEvent } from './firebase';
 
