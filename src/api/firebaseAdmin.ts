738a67515a578bc3d1b86561d6224ebc850e4051
Nathan Edwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-05-21 11:13:36 -0400

    chore: remove unused code

--- src/api/firebaseAdmin.ts
+++ src/api/firebaseAdmin.ts
@@ -16,7 +16,6 @@ import { initializeApp, ServiceAccount } from 'firebase-admin/app';
 
 import { convertToString } from '@/utils/utils';
 import { AuthUserRecord } from '@/types/UserTypes';
-import { imageImports } from '@/data/imports/tag_image_map';
 
 const region = 'us-east1';
 
@@ -67,27 +66,11 @@ function findPaths(fileNames: string[]): string[] {
     const files = fs.readdirSync(directoryPath, { withFileTypes: true });
     for (const file of files) {
         const filePath = path.join(directoryPath, file.name);
-        if (fileNames && fileNames.includes(file.name)) filePaths.push(filePath);
+        if (fileNames && fileNames.includes(file.name)) {
+            filePaths.push(filePath);
+        }
     }
     return filePaths;
-}
-
-//Used for importing images from spreadsheet
-export async function getBase64ImagesFromTagnumber(tagNumber: string) {
-    const fileNames: string[] = imageImports[tagNumber];
-    const filePaths: string[] = findPaths(fileNames);
-    let base64Files = [];
-    for (const filePath of filePaths) {
-        let name = filePath.split('\\').pop()?.split('/').pop() ?? '';
-        const fileBuffer = await fs.promises.readFile(filePath, { encoding: 'base64' });
-        const base64File = {
-            base64Image: fileBuffer,
-            base64ImageName: name,
-            base64ImageType: 'image/jpeg'
-        };
-        base64Files.push(base64File);
-    }
-    return base64Files;
 }
 
 export const addEvent = async (request: any) => {
