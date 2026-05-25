0b28b24d1aa5990568b77a019937eae17f2a56f8
Nathan Edwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-05-25 12:39:31 -0400

    feature(email): permit client-side mail sends

--- src/data/emailSender.ts
+++ src/data/emailSender.ts
@@ -1,3 +1,3 @@
-export const emailSender: string = process.env.EMAIL_SENDER ?? '';
+export const emailSender: string = process.env.NEXT_PUBLIC_EMAIL_SENDER ?? '';
 
-export const emailCc: string = process.env.EMAIL_CC ?? '';
+export const emailCc: string = process.env.NEXT_PUBLIC_EMAIL_CC ?? '';
